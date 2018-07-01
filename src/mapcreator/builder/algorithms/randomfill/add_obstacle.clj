3ea89226c0f37bfdd19ce5f6cc27d40c85077260
ArcQ <eddielaw296@gmail.com>
2018-07-01 19:45:27 -0400

    feat implement add obstacle

--- src/mapcreator/builder/algorithms/randomfill/add_obstacle.clj
+++ src/mapcreator/builder/algorithms/randomfill/add_obstacle.clj
@@ -1,18 +1,19 @@
-(ns mapcreator.builder.algorithms.randomfill.add-obstacle)
+(ns mapcreator.builder.algorithms.randomfill.add-obstacle
+  (:require [mapcreator.builder.shared.utils :as utils]))
 
 (defn randomPoint
   "offset is for pushing the random point so that it will be within a certain quadrant"
-  [offset dimensions gameMap]
-  (randomPoint offset dimensions dimensions gameMap)
-  [offset buffer dimensions gameMap]
+  ([offset dimensions gameMap]
+   (randomPoint offset {:x 0 :y 0} dimensions gameMap))
+  ([offset buffer dimensions gameMap]
 
-  (letfn [(getPointDim [k dim]
-            (-> dim
-                (- (k buffer) (k offset))
-                (+ (k buffer))
-                (rand-int)))]
-    {:y (getPointDim :y (count gameMap))
-     :x (getPointDim :x (count (gameMap 0)))}))
+   (letfn [(getPointDim [k dim]
+             (-> dim
+                 (- (k offset) (k buffer) (k dimensions))
+                 (rand-int)
+                 (+ (k offset))))]
+     {:y (getPointDim :y (count gameMap))
+      :x (getPointDim :x (count (gameMap 0)))})))
 
 (defn makeObstacleVector [{xDim :x yDim :y :as dimensions}]
   (map
@@ -20,22 +21,18 @@
    (range yDim)))
 
 (defn addObstacleToGameMap [gameMap {xDim :x yDim :y :as dimensions} {xPoint :x yPoint :y :as point}]
-  (map-indexed
-   (fn [ydx row]
-     (map-indexed
-      (fn [xdx ele]  (if (and
-                          (>= ydx yPoint)
-                          (< ydx (+ yPoint yDim))
-                          (>= xdx xPoint)
-                          (< xdx (+ xPoint xDim)))
-                       1
-                       0))
-      row))
-   gameMap))
-
-(defn addObstacle [gameMap {xDim :x yDim :y :as dimensions}]
-  (doall (->> (randomPoint {:x 2 :y 2} dimensions gameMap)
-              (addObstacleToGameMap gameMap dimensions))))
+  (vec (map-indexed
+        (fn [ydx row]
+          (vec (map-indexed
+                (fn [xdx ele]  (if (and
+                                    (>= ydx yPoint)
+                                    (< ydx (+ yPoint yDim))
+                                    (>= xdx xPoint)
+                                    (< xdx (+ xPoint xDim)))
+                                 1
+                                 (utils/getLocVal gameMap {:x xdx :y ydx})))
+                row)))
+        gameMap)))
 
 (def testGameMapTwo [[0 0 0 0 0 0 0]
                      [0 0 0 0 0 0 0]
@@ -44,8 +41,19 @@
                      [0 0 0 0 0 0 0]])
 
 (defn getTestGameMapThree []
-  (doall (vec (repeat 27 (vec (doall (repeat 18 0)))))))
+  (vec (repeat 28
+               (vec
+                (repeat 18 0)))))
 
-;; (run! prn (addObstacle testGameMapTwo {:x 2 :y 3}))
-
-(run! prn (addObstacle (getTestGameMapThree) {:x 3 :y 3}))
+(run! prn
+      (let [dimensions {:x 3 :y 3}
+            gameMap (getTestGameMapThree)]
+        (doall (reduce
+                #(->> %1
+                      (randomPoint
+                       (:offset %2)
+                       (:buffer %2)
+                       dimensions)
+                      (addObstacleToGameMap %1 dimensions))
+                gameMap
+                (utils/getBufferOffsetZone gameMap {:x 4 :y 4})))))
